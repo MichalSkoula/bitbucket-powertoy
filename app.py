a633fa3734e5a525de234364ace0f68709570920
MichalSkoula <michal@skoula.cz>
2022-07-14 21:46:18 +0200

    yes, parallel requests => faster app 2

--- app.py
+++ app.py
@@ -26,39 +26,18 @@ def index(what = 'open'):
         # get all repositories
         repositories = get('repositories/' + session['owner'], 'pagelen=100')
 
-        # first, prepare get requests
-        regs = []
-        really_what = what
-        for r in repositories['values']:
-            if what == 'hold':
-                really_what = 'state="on hold"'
-            elif what == 'resolved':
-                really_what = 'state="resolved"'
-            else:
-                really_what = 'state="new" OR state="open"'
-
-            regs.append(
-                grequests.get(
-                    URL + 'repositories/' + session['owner'] + '/' + r['slug'] + '/issues?pagelen=100&sort=-updated_on&q=' + urllib.parse.quote_plus(really_what),
-                    auth=HTTPBasicAuth(session['username'], session['password'])
-                )
-            )
-
-        # fire all get requests at once
-        result = grequests.map(regs);
+        # get all issues - parallel requests
+        issues_collection = load_issues(repositories['values'], what)
 
         # find issues for repos
-        final_issues = []
-        for r in result:
-            issues = r.json()
-            final_issues.append(issues['values']);
-
         for r in repositories['values']:
-            for fi in final_issues:
-                if fi and fi[0]['repository']['full_name'] == r['full_name']:
+            for issues in issues_collection:
+                issues = issues.json()
+                issues = issues['values']
+                if issues and issues[0]['repository']['full_name'] == r['full_name']:
                     # we found issues for this repo
                     r['open_issues'] = []
-                    for i in fi:
+                    for i in issues:
                         # show only my issues....or ALL issues if what=resolved
                         if (show_all_assignment_on_resolved_issues == True and what == 'resolved') or (i['assignee'] != None and i['assignee']['account_id'] == session['account_id']):
                             r['open_issues'].append(i)
@@ -93,8 +72,32 @@ def logout():
 def format_datetime(value):
     return value[0:10]
 
+# sync
 def get(url, query = ''):
     return requests.get(
         URL + url + '?' + query,
         auth=HTTPBasicAuth(session['username'], session['password'])
     ).json()
+
+# async - grequests
+def load_issues(repositories, what):
+    # first, prepare get requests
+    regs = []
+    really_what = what
+    for r in repositories:
+        if what == 'hold':
+            really_what = 'state="on hold"'
+        elif what == 'resolved':
+            really_what = 'state="resolved"'
+        else:
+            really_what = 'state="new" OR state="open"'
+        
+        regs.append(
+            grequests.get(
+                URL + 'repositories/' + session['owner'] + '/' + r['slug'] + '/issues?pagelen=100&sort=-updated_on&q=' + urllib.parse.quote_plus(really_what),
+                auth=HTTPBasicAuth(session['username'], session['password'])
+            )
+        )
+
+    # fire all get requests at once
+    return grequests.map(regs);
