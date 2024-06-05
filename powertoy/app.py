a4aa98d17a4a56e018c03187cb20631c2966b57f
Lukas Bacovsky <l.bacovsky@gmail.com>
2024-06-05 22:42:38 +0200

    set port back to 5000

--- powertoy/app.py
+++ powertoy/app.py
@@ -165,5 +165,5 @@ def load_issues(repositories, issue_state, show_only_critical_issues=False):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='127.0.0.1', port=port, debug=True)
