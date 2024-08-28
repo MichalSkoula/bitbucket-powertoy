0ac91d6a966e784d62c6f0a316e6a7d736311159
Lukáš Bačovský <l.bacovsky@gmail.com>
2024-08-28 18:15:18 +0200

    hopefully working SHOW_ALL_ASSIGNMENTS_ON_RESOLVED_ISSUES_PAGE setting

--- powertoy/app.py
+++ powertoy/app.py
@@ -10,16 +10,16 @@ app = Flask(__name__)
 
 # SETTINGS START #
 URL = "https://api.bitbucket.org/2.0/"
-app.secret_key = 'asdasdadadsasd'
-show_all_assignment_on_resolved_issues = True
+app.secret_key = "asdasdadadsasd"
+SHOW_ALL_ASSIGNMENTS_ON_RESOLVED_ISSUES_PAGE = False
 # SETTINGS END #
 
 
 @app.route("/")
 @app.route("/<what>")
-def index(what='open'):
-    if 'username' not in session:
-        return render_template('login.html')
+def index(what="open"):
+    if "username" not in session:
+        return render_template("login.html")
 
     # some stats variables
     user_issue_count = {}
@@ -28,91 +28,113 @@ def index(what='open'):
 
     # get all repositories
     try:
-        repositories = fetch_repository_data('repositories/' + session['owner'], 'pagelen=100')
+        repositories = fetch_repository_data(
+            "repositories/" + session["owner"], "pagelen=100"
+        )
     except ValueError as e:
         flash(str(e))
-        return redirect(url_for('logout'))
+        return redirect(url_for("logout"))
 
     # get all issues - parallel requests
-    issues_collection = load_issues(repositories['values'], what, session['critical'])
+    issues_collection = load_issues(repositories["values"], what, session["critical"])
 
     # find issues for repos
-    for repository in repositories['values']:
-
+    for repository in repositories["values"]:
         for issues in issues_collection:
-            
-            issues = issues.json().get('values')
-            if not issues or not issues[0]['repository']['full_name'] == repository['full_name']:
+            issues = issues.json().get("values")
+            if (
+                not issues
+                or not issues[0]["repository"]["full_name"] == repository["full_name"]
+            ):
                 # no issues or repo does not have issue tracker?
                 continue
 
             # we found issues for this repo
-            repository['open_issues'] = []
-            repositories_with_issues[repository['full_name']] = len(issues)
+            repository["open_issues"] = []
+            repositories_with_issues[repository["full_name"]] = len(issues)
 
             for issue in issues:
-                repository['open_issues'].append(issue)
-
-                if issue['assignee']:
+                if issue["assignee"]:
                     # Check if the key exists in the dictionary, if not, return 0 and add 1
-                    user_issue_count[issue['assignee']['display_name']] = user_issue_count.get(
-                        issue['assignee']['display_name'], 0) + 1
+                    user_issue_count[issue["assignee"]["display_name"]] = (
+                        user_issue_count.get(issue["assignee"]["display_name"], 0) + 1
+                    )
 
-                if (show_all_assignment_on_resolved_issues is not True or what != 'resolved') and (
-                        issue['assignee'] is None or issue['assignee']['account_id'] != session['account_id']):
-                    # show only my issues....or ALL issues if what=resolved
+                # show only my issues....or ALL issues if what=resolved
+                if (
+                    SHOW_ALL_ASSIGNMENTS_ON_RESOLVED_ISSUES_PAGE is False
+                    or what != "resolved"
+                ) and (
+                    issue["assignee"]
+                    and issue["assignee"]["account_id"] != session["account_id"]
+                ):
                     continue
 
+                repository["open_issues"].append(issue)
                 issues_count += 1
 
     # sort user_issue_count by value desc
-    user_issue_count = dict(sorted(user_issue_count.items(), key=lambda item: item[1], reverse=True))
+    user_issue_count = dict(
+        sorted(user_issue_count.items(), key=lambda item: item[1], reverse=True)
+    )
 
     # sort repositories_with_issues by value desc
-    repositories_with_issues = dict(sorted(repositories_with_issues.items(), key=lambda item: item[1], reverse=True))
+    repositories_with_issues = dict(
+        sorted(repositories_with_issues.items(), key=lambda item: item[1], reverse=True)
+    )
 
-    return render_template('index.html', repositories=repositories['values'], what=what, issues_count=issues_count,
-                           user_issue_count=user_issue_count, repositories_with_issues=repositories_with_issues)
+    return render_template(
+        "index.html",
+        repositories=repositories["values"],
+        what=what,
+        issues_count=issues_count,
+        user_issue_count=user_issue_count,
+        repositories_with_issues=repositories_with_issues,
+    )
 
 
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
     required_form_data = (
-        request.form.get('username'),
-        request.form.get('password'),
+        request.form.get("username"),
+        request.form.get("password"),
     )
     if not all(required_form_data):
         flash("Please enter username and password")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
     try:
-        session['username'] = request.form.get('username')
-        session['password'] = request.form.get('password')
-        session['critical'] = request.form.get('critical') is not None
-        session['owner'] = request.form.get('owner') if request.form.get('owner') else request.form.get('username')
+        session["username"] = request.form.get("username")
+        session["password"] = request.form.get("password")
+        session["critical"] = request.form.get("critical") is not None
+        session["owner"] = (
+            request.form.get("owner")
+            if request.form.get("owner")
+            else request.form.get("username")
+        )
 
-        response = fetch_repository_data('user')
+        response = fetch_repository_data("user")
     except ValueError:
         # wrong credentials or repository data fetching failed
         # because of TimeoutError, NetworkError etc.
         # So it is possible that we are lying to the user here
-        flash('Username or Password is wrong')
+        flash("Username or Password is wrong")
     else:
-        acc_id = response.get('account_id')
+        acc_id = response.get("account_id")
         if not acc_id:
             raise ValueError("Cannot set account ID")
 
-        session['account_id'] = acc_id
-        flash('You were successfully logged in')
-    return redirect(url_for('index'))
+        session["account_id"] = acc_id
+        flash("You were successfully logged in")
+    return redirect(url_for("index"))
 
 
-@app.route("/logout", methods=['GET'])
+@app.route("/logout", methods=["GET"])
 def logout():
-    session.pop('username', None)
-    session.pop('password', None)
-    flash('You were successfully logged out')
-    return redirect(url_for('index'))
+    session.pop("username", None)
+    session.pop("password", None)
+    flash("You were successfully logged out")
+    return redirect(url_for("index"))
 
 
 @app.template_filter()
@@ -121,14 +143,20 @@ def format_datetime(value):
 
 
 # sync
-def fetch_repository_data(url, query=''):
-    credentials = (session.get('username'), session.get('password'))
+def fetch_repository_data(url, query=""):
+    credentials = (session.get("username"), session.get("password"))
     if not all(credentials):
         raise ValueError("Credentials were not provided")
 
-    response = requests.get(url=URL + url + '?' + query, auth=HTTPBasicAuth(*credentials))
+    response = requests.get(
+        url=URL + url + "?" + query, auth=HTTPBasicAuth(*credentials)
+    )
     if not response.ok:
-        raise ValueError("Bad response - status code: {}\n{}".format(response.status_code, response.text))
+        raise ValueError(
+            "Bad response - status code: {}\n{}".format(
+                response.status_code, response.text
+            )
+        )
     return response.json()
 
 
@@ -138,8 +166,8 @@ def load_issues(repositories, issue_state, only_critical_and_blocker=False):
     regs = []
     DEFAULT_QUERY = '(state="new" OR state="open")'
     ISSUE_STATES_MAP = {
-        'hold': '(state="on hold")',
-        'resolved': '(state="resolved")',
+        "hold": '(state="on hold")',
+        "resolved": '(state="resolved")',
     }
 
     for repository in repositories:
@@ -151,9 +179,14 @@ def load_issues(repositories, issue_state, only_critical_and_blocker=False):
 
         regs.append(
             grequests.get(
-                URL + 'repositories/' + session['owner'] + '/' + repository[
-                    'slug'] + '/issues?pagelen=100&sort=-updated_on&q=' + urllib.parse.quote_plus(current_issue_state),
-                auth=HTTPBasicAuth(session['username'], session['password'])
+                URL
+                + "repositories/"
+                + session["owner"]
+                + "/"
+                + repository["slug"]
+                + "/issues?pagelen=100&sort=-updated_on&q="
+                + urllib.parse.quote_plus(current_issue_state),
+                auth=HTTPBasicAuth(session["username"], session["password"]),
             )
         )
 
@@ -163,4 +196,4 @@ def load_issues(repositories, issue_state, only_critical_and_blocker=False):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host="127.0.0.1", port=port, debug=True)
